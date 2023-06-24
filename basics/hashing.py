9f6d53cdf12d0c8f06703ea4cd3274e2765c79ef
Al <albarrentine@gmail.com>
2023-06-24 02:37:35 -0400

    [utils] adding crc32_unsigned

---
+++ basics/hashing.py
@@ -0,0 +1,7 @@
+import binascii
+from basics.encoding import safe_encode
+
+
+def crc32_unsigned(value):
+    '''binascii's crc32 is signed, make it unsigned'''
+    return binascii.crc32(safe_encode(value)) & 0xffffffff
