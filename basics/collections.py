383388fae6d51a7f5a7085fcdbf5602b141f76ed
Al <albarrentine@gmail.com>
2023-06-24 02:14:44 -0400

    [utils] adding collections with is_mapping, etc.

---
+++ basics/collections.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+from collections.abc import Mapping, Hashable
+
+
+def unique_list(values):
+    """ Produces a unique list while preserving input order """
+    return list(OrderedDict.fromkeys(values))
+
+
+def is_mapping(val):
+    return isinstance(val, Mapping)
+
+def is_hashable(val):
+    return isinstance(val, Hashable)
