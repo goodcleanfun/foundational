fc4ac9b5e034b172f5e584a0fea3607f5c688272
Al <albarrentine@gmail.com>
2023-06-25 01:12:55 -0400

    [test] tests for AttrDict

--- basics/attrdict.py
+++ basics/attrdict.py
@@ -9,7 +9,7 @@ class AttrDict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
-    def __delattr__(self, key, value):
+    def __delattr__(self, key):
         del self[key]
 
     def __repr__(self):
---
+++ tests/test_attrdict.py
@@ -0,0 +1,10 @@
+from basics.attrdict import AttrDict
+
+def test_attrdict():
+    a = AttrDict()
+    a.key = "value"
+    assert a.key == "value"
+    assert a["key"] == "value"
+
+    del(a.key)
+    assert "key" not in a
