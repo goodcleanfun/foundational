c02dc2e07101f9f20c0b83f1f1475d57995ee4f1
Al <albarrentine@gmail.com>
2024-01-14 00:16:48 -0500

    [fix] logic on downloading file where the URL is not obviously a file but destination is

--- foundational/download.py
+++ foundational/download.py
@@ -17,10 +17,11 @@ def download_file(url, dest, retries=3, retry_delay=5, session=requests, headers
     url = furl.furl(url)
 
     exists = os.path.exists(dest)
-    is_dir = os.path.isdir(dest) if exists else len(os.path.splitext(dest)[1]) > 0
+    is_dir = os.path.isdir(dest) if exists else len(os.path.splitext(dest)[1]) == 0
 
     base_dir = dest if is_dir else os.path.dirname(dest)
-    ensure_dir(base_dir)
+    if base_dir:
+        ensure_dir(base_dir)
 
     dest_filename = os.path.basename(dest) if not is_dir else None
     if not dest_filename and url.path.isfile:
